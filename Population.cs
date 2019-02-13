1a92d89e831e6646227bb0a8c66146033469caec
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-13 23:57:31 +0300

    FixBugs

--- Population.cs
+++ Population.cs
@@ -6,6 +6,8 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
+            if (initialPopulation <= 0 || currentPopulation <= 0)
+                throw new ArgumentException();
             double value = initialPopulation;
             int yearsNum = 0;
             for (yearsNum = 0; value <= currentPopulation; yearsNum++)
