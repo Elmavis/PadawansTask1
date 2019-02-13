1620a54df7cb661324570ec1df903810e4037798
Elmavis <37665679+Elmavis@users.noreply.github.com>
2019-02-13 23:25:08 +0300

    LastCommit

--- Population.cs
+++ Population.cs
@@ -6,8 +6,14 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            double value = initialPopulation;
+            int yearsNum = 0;
+            for (yearsNum = 0; value <= currentPopulation; yearsNum++)
+            {
+                value += value * percent * 0.01;
+                value += visitors;
+            }
+            return yearsNum;
         }
     }
 }
